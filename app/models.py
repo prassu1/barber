e216ef536fc2f90db90bcb7204f2e44bce272df2
prassu1 <bollaprasanna76@gmail.com>
2025-03-07 10:06:00 +0530

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils import timezone
+from decimal import Decimal
+import datetime
 
 
 #custom user manager
@@ -41,18 +43,23 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
 
 
 class Service(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-
+    name = models.CharField(max_length=225)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True) 
+    # menu_item = models.ForeignKey(on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return self.name
+    
     
 class Booking(models.Model):
     service = models.ForeignKey(Service, related_name="bookings", on_delete=models.CASCADE)
     frequency = models.CharField(max_length=255)
-    duration = models.IntegerField()
+    duration = models.PositiveIntegerField()
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    total_paid = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     start_datetime = models.DateTimeField(default=timezone.now)
+    installment_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    
+
     
     frequency_choices = [
         ('weekly', 'Weekly'),
@@ -70,11 +77,65 @@ class Booking(models.Model):
     total_cost = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     
 def __str__(self):
-        return f"{self.service.name} - {self.frequency} for {self.duration} months , Booking time: {self.start_datetime} "
+    return f"{self.service.name} - {self.frequency} for {self.duration} months , Booking time: {self.start_datetime} "
+    
 
+
+class Payment(models.Model):
+    booking = models.ForeignKey('Booking', related_name='payments', on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=255)
+    payment_status = models.CharField(max_length=50, default='Pending')
+    payment_type = models.CharField(max_length=50, default='full')
+    paid_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    due_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    remaining_paid_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    partial_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    installment_plan = models.BooleanField(default=False)
+    installment_number = models.PositiveIntegerField(null=True, blank=True)
+    frequency = models.CharField(max_length=20, null=True, blank=True)
+    paid_datetime = models.DateTimeField(default=datetime.datetime.now) 
+    due_months = models.IntegerField(null=True)
+    total_paid = models.DecimalField(max_digits=10, decimal_places=2 , null=True)
+    appointment = models.ForeignKey('AppointmentBooking', on_delete=models.CASCADE, related_name='payments', null=True, blank=True)  # Corrected the ForeignKey
+
+    PAYMENT_METHOD_CHOICES = [
+        ('credit_card', 'Credit Card'),
+        ('paypal', 'PayPal'),
+        ('bank_transfer', 'Bank Transfer'),
+    ]
+    PAYMENT_STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed'),
+    ]
+    PAYMENT_TYPE_CHOICES = [
+        ('full', 'Full Payment'),
+        ('partial', 'Partial'),
+        ('installment', 'Installment'),
+    ]
+
+    def __str__(self):
+        return f"Payment {self.id} for Appointment {self.appointment.id}" if self.appointment else f"Payment {self.id}"
+
+        
  
-
-
+class AppointmentBooking(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('canceled', 'Canceled'),
+        
+    ]
+    
+    booking_id = models.CharField(max_length=255)
+    appointment_date = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    customer_email = models.EmailField(null=True)
+    
+    def __str__(self):
+        return f"AppointmentBooking {self.booking_id} on {self.appointment_date}"
     
 
 
--- app/serializers.py
+++ app/serializers.py
@@ -10,10 +10,14 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
-from .models import Booking,Service
+from .models import Booking,Service,Payment
 from decimal import Decimal
 from datetime import timedelta
 from django.utils import timezone
+from django.db.models import Sum
+from .models import AppointmentBooking
+from django.core.mail import send_mail
+ 
 
 
 
@@ -120,19 +124,16 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = CustomUser  
         fields = ['username', 'email'] 
     
-    
-
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = ['id', 'name', 'price']
 
-
 class BookingSerializer(serializers.ModelSerializer):
     service = serializers.PrimaryKeyRelatedField(queryset=Service.objects.all())  
     class Meta:
         model = Booking
-        fields = ['id', 'service', 'frequency', 'duration', 'total_cost', 'start_datetime']
+        fields = ['id', 'service', 'frequency', 'duration', 'total_cost', 'start_datetime', 'total_paid']
     
     def create(self, validated_data):
         service = validated_data.get('service')  
@@ -146,24 +147,251 @@ class BookingSerializer(serializers.ModelSerializer):
             frequency=frequency,
             duration=duration,
             total_cost=total_cost,
-            start_datetime=start_datetime
+            start_datetime=start_datetime,
+            total_paid=Decimal('0.00')
         )
         return booking
 
     def calculate_total_cost(self, service, frequency, duration):
-        discount = Decimal('0.10') if duration >= 3 else Decimal('0.00')  
+        total_sessions = 5 
+        discount = Decimal('0.10') if duration == 3 and total_sessions == 5 else Decimal('0.00')
         if frequency == 'weekly':
-            total_sessions = duration * 4  
+            total_sessions = duration * 4 
+            unit_price = service.price_per_week  
         elif frequency == 'bi-weekly':
-            total_sessions = duration * 2  
+            total_sessions = duration * 2
+            unit_price = service.price_per_biweekly 
         elif frequency == 'monthly':
             total_sessions = duration 
+            unit_price = service.price_per_month
+        else:
+            total_sessions = 0 
+            unit_price = Decimal('0.00')
         
-        total_cost = Decimal(service.price) * Decimal(total_sessions)  
+        total_cost = unit_price * total_sessions 
         total_cost *= (Decimal('1.00') - discount)  
         return total_cost
+    
+
+class PaymentSerializer(serializers.ModelSerializer):
+    booking = serializers.PrimaryKeyRelatedField(queryset=Booking.objects.all())
+    paid_amount = serializers.SerializerMethodField()
+    due_amount = serializers.SerializerMethodField()
+    remaining_paid_amount = serializers.SerializerMethodField()  
+    total_paid = serializers.SerializerMethodField() 
+
+    class Meta:
+        model = Payment
+        fields = ['id', 'booking', 'amount', 'payment_method', 'payment_status', 'payment_type', 'partial_amount', 'installment_plan', 'installment_number', 'paid_amount', 'due_amount', 'remaining_paid_amount' , 'paid_datetime', 'due_months', 'total_paid']
+    
+    def get_paid_amount(self, obj):
+        total_paid = Payment.objects.filter(booking=obj.booking).aggregate(
+            total_paid=Sum('amount')
+        )['total_paid'] or Decimal('0.00')
+        return total_paid 
+        
+    def get_due_amount(self, obj): 
+        total_paid = self.get_paid_amount(obj)
+        due_amount = obj.booking.total_cost - total_paid
+        return max(Decimal('0.00'), due_amount)
+        
+    def get_remaining_paid_amount(self, obj):
+        total_paid = self.get_paid_amount(obj)
+        remaining_paid_amount = obj.booking.total_cost - total_paid
+        return max(Decimal('0.00'), remaining_paid_amount)
+
+    def get_total_paid(self, obj): 
+        total_paid = Payment.objects.filter(booking=obj.booking).aggregate(
+            total_paid=Sum('amount')
+        )['total_paid'] or Decimal('0.00')
+        return total_paid
+    
+    
+    def create(self, validated_data):
+        booking = validated_data['booking']
+        amount = validated_data['amount']
+        payment_method = validated_data['payment_method']
+        payment_type = validated_data.get('payment_type', 'full').lower() 
+        partial_amount = validated_data.get('deposit_amount', 0)
+        installment_plan = validated_data.get('installment_plan', False)  
+        due_months = validated_data.get('due_months', None)  
+        total_paid = validated_data.get('total_paid', Decimal('0.00'))
+        installment_number = None 
+        
+        if payment_type == 'full' :
+            if booking.total_paid >= booking.total_cost:
+                ({"amount": ["This booking has already been fully paid. No further full payments are allowed."]})
+            if amount != booking.total_cost:
+                raise serializers.ValidationError({"amount": ["Amount must match the total cost of the booking for full payment."]})
+        
+        elif payment_type == 'partial':
+            if amount <= 0:
+                raise serializers.ValidationError({"amount": ["Partial payment amount must be greater than 0."]})
+            if booking.total_paid >= booking.total_cost:
+                raise serializers.ValidationError({"amount": ["This booking has already been fully paid. No further partial payments are allowed."]})
+            if booking.total_paid + amount > booking.total_cost:
+                raise serializers.ValidationError({"amount": ["Partial payment exceeds the total cost of the booking."]})
+        
+        elif payment_type == 'installment' :
+            if not installment_plan :
+                raise serializers.ValidationError({"installment_plan": ["Installment plan must be enabled and the installment number specified."]})
+            if due_months is None or due_months <= 0:
+                raise serializers.ValidationError({"due_months": ["Due months must be specified and greater than 0."]})
+            
+            installment_amount = booking.total_cost / due_months
+            if amount != installment_amount:
+                raise serializers.ValidationError({"amount": [f"Installment amount must be {installment_amount} per month."]})
+            
+            previous_installments = Payment.objects.filter(
+                booking=booking,
+                payment_type='installment'
+            ).count() 
+            installment_number = previous_installments + 1
+            
+            if installment_number > due_months:
+                raise serializers.ValidationError({"installment_number": [f"Installment number cannot exceed {due_months}. This booking only allows {due_months} installments."]})
+
+            
+        if payment_type == 'full' and amount == booking.total_cost:
+            payment_status = 'Completed'
+        else:
+            payment_status = 'pending'
+
+            payment_date = timezone.now()
+
+        payment = Payment.objects.create(
+            booking=booking,
+            amount=amount,
+            payment_method=payment_method,
+            payment_status=payment_status,
+            payment_type=payment_type,
+            partial_amount=partial_amount,
+            installment_plan=installment_plan,
+            installment_number=installment_number, 
+            due_months=due_months,
+        )
+
+        if payment_status == 'Completed' and payment_type == 'full':
+            booking.total_cost = amount
+            booking.save()
+
+        if payment_type == 'installment': 
+            installment_amount = booking.total_cost / due_months
+            booking.total_paid += installment_amount
+            booking.save()
+
+        if payment_type == 'partial': 
+            booking.total_paid += amount
+            booking.save()
+
+        payment.total_paid = self.get_total_paid(payment)  
+        payment.paid_amount = self.get_paid_amount(payment)  
+        payment.due_amount = self.get_due_amount(payment)  
+        payment.remaining_paid_amount = self.get_remaining_paid_amount(payment)  
+        payment.save()  
+        return payment
+    
+
+class AppointmentBookingSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)  
+
+    class Meta:
+        model = AppointmentBooking
+        fields = ['booking_id', 'appointment_date', 'payments','customer_email']  
+
+    def create(self, validated_data):
+        appointment = AppointmentBooking.objects.create(**validated_data)
+        existing_payments = Payment.objects.filter(booking_id=appointment.booking_id)
+        for payment in existing_payments:
+            payment.appointment = appointment
+            payment.save()  
+
+        full_payment = any(payment.payment_status == 'Completed' and payment.due_amount == 0 for payment in existing_payments)
+        
+        if full_payment:
+            appointment.status = 'confirmed'
+            appointment.save()
+            response = {
+                'message': 'Appointment booked successfully with full payment.',
+                'appointment_id': appointment.id,
+                'status': appointment.status
+            }
+            self.send_confirmation_email(appointment)
+            print('hi')
+        else:
+            appointment.status = 'pending'
+            appointment.save()
+            response = {
+                'message': 'Appointment booked with pending payment.',
+                'appointment_id': appointment.id,
+                'status': appointment.status
+            }
+
+        return response
+
+    def cancel_appointment(self, appointment_id):
+        try:
+            appointment = AppointmentBooking.objects.get(id=appointment_id)
+            appointment.status = 'canceled'
+            appointment.save()
+
+            self.send_cancellation_email(appointment)
+
+            return {
+                'message': 'Appointment canceled successfully.',
+                'appointment_id': appointment.id,
+                'status': appointment.status
+            }
+        except AppointmentBooking.DoesNotExist:
+            return {
+                'message': 'Appointment not found.',
+                'appointment_id': appointment_id
+            }
+
+    def send_confirmation_email(self, appointment):
+        user_email = appointment.customer_email  
+        subject = 'Your Appointment has been Confirmed'
+        message = f"Dear {'user'},\n\n" \
+                  f"Your appointment has been successfully booked and confirmed for {appointment.appointment_date}. " \
+                  f"Thank you for making the full payment.\n\n" \
+                  f"Best regards,\nYour Company Name"
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,  
+            [user_email],              
+            fail_silently=False
+        )
+
+    def send_cancellation_email(self, appointment):
+        user_email = appointment.customer_email  
+        subject = 'Your Appointment has been Canceled'
+        message = f"Dear {'user'},\n\n" \
+                  f"Your appointment scheduled for {appointment.appointment_date} has been canceled. " \
+                  f"We are sorry for the inconvenience.\n\n" \
+                  f"Best regards,\nYour Company Name"
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,  
+            [user_email],              
+            fail_silently=False
+        ) 
 
 
+
+
+    
+    
+    
+
+
+
+    
+        
+        
 
     
     
